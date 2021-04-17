d1ed45829d30c023e09402f4a935d1996cc436f9
andreapasetti <andrea.pasetti@outlook.com>
2021-04-17 10:12:19 +0000

    Final project

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,9 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/workspace/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+<<<<<<< HEAD
+underlay_workspaces = "/home/workspace/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/workspace/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+=======
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
+>>>>>>> 1ae73f7a4d41ad232e2ba54245c6e7a451386241
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+<<<<<<< HEAD
+export PWD="/home/workspace/catkin_ws/build"
+=======
+export CMAKE_PREFIX_PATH="/home/workspace/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/workspace/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/workspace/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/workspace/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/workspace/catkin_ws/src:$ROS_PACKAGE_PATH"
+>>>>>>> 1ae73f7a4d41ad232e2ba54245c6e7a451386241
